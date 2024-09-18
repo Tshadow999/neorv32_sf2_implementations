59d5188503c6ed782276531cbf999a5f5f8a7422
Tycho Schurink <tychoschurink@outlook.com>
2024-09-18 10:31:50 +0200

    Added neorv32 as a submodule and changed some of the names in the import_hdl_sources.tcl

--- neorv32-sf2-with-envm/import_hdl_sources.tcl
+++ neorv32-sf2-with-envm/import_hdl_sources.tcl
@@ -8,17 +8,20 @@ import_files -library neorv32 -hdl_source ./custom_rtl/neorv32_package.vhd
 #import_files -library neorv32 -hdl_source ./neorv32_application_image.vhd
 import_files -library neorv32 -hdl_source ${neorv_rtl_dir}/core/neorv32_bootloader_image.vhd
 
-import_files -library neorv32 -hdl_source ${neorv_rtl_dir}/core/neorv32_dmem.entity.vhd
-import_files -library neorv32 -hdl_source ${neorv_rtl_dir}/core/neorv32_imem.entity.vhd
+# removed the .entity names from the files because those dont exist
+import_files -library neorv32 -hdl_source ${neorv_rtl_dir}/core/neorv32_dmem.vhd
+import_files -library neorv32 -hdl_source ${neorv_rtl_dir}/core/neorv32_imem.vhd
 
 foreach file [glob ${neorv_rtl_dir}/core/*.vhd] {
-    if {[file tail $file] != "neorv32_package.vhd" && [file tail $file] != "neorv32_application_image.vhd" && [file tail $file] != "neorv32_bootloader_image.vhd" && [file tail $file] != "neorv32_imem.entity.vhd" && [file tail $file] != "neorv32_dmem.entity.vhd"} {
+    if {[file tail $file] != "neorv32_package.vhd" && [file tail $file] != "neorv32_application_image.vhd" && [file tail $file] != "neorv32_bootloader_image.vhd" && [file tail $file] != "neorv32_imem.entity.vhd" && [file tail $file] != "neorv32_dmem.vhd"} {
         import_files -library neorv32 -hdl_source $file 
         #check_hdl -file ${prj_dir_name}/hdl/[file tail $file]
     }
 }
 build_design_hierarchy
-foreach file [glob ${neorv_rtl_dir}/core/mem/*.default.vhd] {
-    import_files -library neorv32 -hdl_source $file
-}
+
+# There is no mem directory in core
+# foreach file [glob ${neorv_rtl_dir}core/mem/*.default.vhd] {
+#     import_files -library neorv32 -hdl_source $file
+# }
 build_design_hierarchy
