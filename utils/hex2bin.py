16a51770842d484e4802946f4ee0cbaf9f8f70c0
Tycho <tychoschurink@outlook.com>
2024-10-23 17:08:58 +0200

    Added the hex to bin converter

--- utils/hex2bin.py
+++ utils/hex2bin.py
@@ -1,10 +1,33 @@
-input = "../neorv32-sf2-with-envm/neorv32/sw/example/demo_blink_led/main.c";
-output = "output.mem";
+input = "neorv32_raw_exe.hex";
+output = open("neorv32_program.mem", "w");
+
+lookup = {
+    "0" : "0000",
+    "1" : "0001",
+    "2" : "0010",
+    "3" : "0011",
+    "4" : "0100",
+    "5" : "0101",
+    "6" : "0110",
+    "7" : "0111",
+    "8" : "1000",
+    "9" : "1001",
+    "a" : "1010",
+    "b" : "1011",
+    "c" : "1100",
+    "d" : "1101",
+    "e" : "1110",
+    "f" : "1111",
+    "\n": "\n",
+}
 
 def hex2bin():
     with open(input) as file:
         for line in file:
-            print(line.strip())
+            for char in line:
+                output.write(lookup[char]);
+            pass;
+    output.close();
 
 
 if __name__ == "__main__":
