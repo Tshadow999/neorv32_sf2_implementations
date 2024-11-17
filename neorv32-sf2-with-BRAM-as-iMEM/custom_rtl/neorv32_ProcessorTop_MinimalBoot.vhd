ef3dc493b8c1087f7a87747f2df8308a4db5d8a9
Tycho <tychoschurink@outlook.com>
2024-11-17 14:19:21 +0100

    Updated the benchmark

--- neorv32-sf2-with-BRAM-as-iMEM/custom_rtl/neorv32_ProcessorTop_MinimalBoot.vhd
+++ neorv32-sf2-with-BRAM-as-iMEM/custom_rtl/neorv32_ProcessorTop_MinimalBoot.vhd
@@ -17,7 +17,7 @@ library neorv32;
 entity neorv32_ProcessorTop_MinimalBoot is
   generic (
     -- General --
-    CLOCK_FREQUENCY   : natural := 10e6;       -- clock frequency of clk_i in Hz
+    CLOCK_FREQUENCY   : natural := 50e6;       -- clock frequency of clk_i in Hz
     INT_BOOTLOADER_EN : boolean := true;  -- boot configuration: true = boot explicit bootloader; false = boot from int/ext (I)MEM
     -- Internal Instruction memory --
     MEM_INT_IMEM_EN   : boolean := true;    -- implement processor-internal instruction memory
@@ -35,19 +35,19 @@ entity neorv32_ProcessorTop_MinimalBoot is
     -- RISC-V CPU Extensions --
     CPU_EXTENSION_RISCV_A      : boolean := false;  -- implement atomic memory operations extension?
     CPU_EXTENSION_RISCV_B      : boolean := false;  -- implement bit-manipulation extension?
-    CPU_EXTENSION_RISCV_C      : boolean := false;  -- implement compressed extension?
+    CPU_EXTENSION_RISCV_C      : boolean := true;  -- implement compressed extension?
     CPU_EXTENSION_RISCV_E      : boolean := false;  -- implement embedded RF extension?
-    CPU_EXTENSION_RISCV_M      : boolean := false;  -- implement mul/div extension?
+    CPU_EXTENSION_RISCV_M      : boolean := true;  -- implement mul/div extension?
     CPU_EXTENSION_RISCV_U      : boolean := false;  -- implement user mode extension?
-    CPU_EXTENSION_RISCV_Zfinx  : boolean := false;  -- implement 32-bit floating-point extension (using INT regs!)
+    CPU_EXTENSION_RISCV_Zfinx  : boolean := true;  -- implement 32-bit floating-point extension (using INT regs!)
     CPU_EXTENSION_RISCV_Zicntr : boolean := true;   -- implement base counters?
     CPU_EXTENSION_RISCV_Zicond : boolean := false;  -- implement integer conditional operations?
     CPU_EXTENSION_RISCV_Zihpm  : boolean := true;   -- implement hardware performance monitors?
     CPU_EXTENSION_RISCV_Zmmul  : boolean := false;  -- implement multiply-only M sub-extension?
     CPU_EXTENSION_RISCV_Zxcfu  : boolean := false;  -- implement custom (instr.) functions unit?
     -- Tuning Options --
-    FAST_MUL_EN                : boolean := false;  -- use DSPs for M extension's multiplier
-    FAST_SHIFT_EN              : boolean := false;  -- use barrel shifter for shift operations
+    FAST_MUL_EN                : boolean := true;  -- use DSPs for M extension's multiplier
+    FAST_SHIFT_EN              : boolean := true;  -- use barrel shifter for shift operations
 
     -- Hardware Performance Monitors (HPM) --
     HPM_NUM_CNTS               : natural range 0 to 13          := 13;           -- number of implemented HPM counters (0..13)
