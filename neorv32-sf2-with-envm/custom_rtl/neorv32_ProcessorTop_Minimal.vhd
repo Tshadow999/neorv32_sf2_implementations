2ea723ef9e1ebbffd5cfa3c3f882983c07e66456
Tycho <tychoschurink@outlook.com>
2024-11-19 14:45:34 +0100

    Done some benchmarking

--- neorv32-sf2-with-envm/custom_rtl/neorv32_ProcessorTop_Minimal.vhd
+++ neorv32-sf2-with-envm/custom_rtl/neorv32_ProcessorTop_Minimal.vhd
@@ -17,10 +17,11 @@ library neorv32;
 entity neorv32_ProcessorTop_Minimal is
   generic (
     -- General --
-    CLOCK_FREQUENCY            : natural := 50e6;   -- clock frequency of clk_i in Hz
+    CLOCK_FREQUENCY            : natural := 100e6;   -- clock frequency of clk_i in Hz
     INT_BOOTLOADER_EN          : boolean := false;  -- boot configuration: true = boot explicit bootloader; false = boot from int/ext (I)MEM
     -- Internal Instruction memory --
     MEM_INT_IMEM_EN            : boolean := false;  -- implement processor-internal instruction memory
+    MEM_INT_IMEM_SIZE          : natural := 16*1024; --  
     -- Internal Data memory --
     MEM_INT_DMEM_EN            : boolean := true;   -- implement processor-internal data memory
     MEM_INT_DMEM_SIZE          : natural := 8*1024; -- size of processor-internal data memory in bytes
@@ -31,24 +32,25 @@ entity neorv32_ProcessorTop_Minimal is
     -- RISC-V CPU Extensions --
     CPU_EXTENSION_RISCV_A      : boolean := false;  -- implement atomic memory operations extension?
     CPU_EXTENSION_RISCV_B      : boolean := false;  -- implement bit-manipulation extension?
-    CPU_EXTENSION_RISCV_C      : boolean := true;  -- implement compressed extension?
+    CPU_EXTENSION_RISCV_C      : boolean := false;  -- implement compressed extension?
     CPU_EXTENSION_RISCV_E      : boolean := false;  -- implement embedded RF extension?
-    CPU_EXTENSION_RISCV_M      : boolean := true;  -- implement mul/div extension?
+    CPU_EXTENSION_RISCV_M      : boolean := false;  -- implement mul/div extension?
     CPU_EXTENSION_RISCV_U      : boolean := false;  -- implement user mode extension?
-    CPU_EXTENSION_RISCV_Zfinx  : boolean := true;  -- implement 32-bit floating-point extension (using INT regs!)
+    CPU_EXTENSION_RISCV_Zfinx  : boolean := false;  -- implement 32-bit floating-point extension (using INT regs!)
     CPU_EXTENSION_RISCV_Zicntr : boolean := true;   -- implement base counters?
     CPU_EXTENSION_RISCV_Zicond : boolean := false;  -- implement integer conditional operations?
     CPU_EXTENSION_RISCV_Zihpm  : boolean := true;  -- implement hardware performance monitors?
     CPU_EXTENSION_RISCV_Zmmul  : boolean := false;  -- implement multiply-only M sub-extension?
     CPU_EXTENSION_RISCV_Zxcfu  : boolean := false;  -- implement custom (instr.) functions unit?
     -- Tuning Options --
-    FAST_MUL_EN                : boolean := true;  -- use DSPs for M extension's multiplier
-    FAST_SHIFT_EN              : boolean := true;  -- use barrel shifter for shift operations
+    FAST_MUL_EN                : boolean := false;  -- use DSPs for M extension's multiplier
+    FAST_SHIFT_EN              : boolean := false;  -- use barrel shifter for shift operations
     -- External bus interface (XBUS) --
     XBUS_EN                    : boolean := true;   -- implement external memory bus interface?
     XBUS_TIMEOUT               : natural := 255;    -- cycles after a pending bus access auto-terminates (0 = disabled)
     -- HPM --
-    HPM_NUM_CNTS               : natural range 0 to 13 := 13
+    HPM_NUM_CNTS               : natural range 0 to 13 := 13;
+    HPM_CNT_WIDTH              : natural range 0 to 64 := 40 -- total size of HPM counters (0..64)
   );
   port (
     -- Global control --
@@ -89,6 +91,7 @@ begin
     INT_BOOTLOADER_EN          => INT_BOOTLOADER_EN,
     -- Internal Instruction memory --
     MEM_INT_IMEM_EN            => MEM_INT_IMEM_EN,
+    MEM_INT_IMEM_SIZE          => MEM_INT_IMEM_SIZE,
     -- Internal Data memory --
     MEM_INT_DMEM_EN            => MEM_INT_DMEM_EN,
     MEM_INT_DMEM_SIZE          => MEM_INT_DMEM_SIZE,
@@ -116,7 +119,8 @@ begin
     XBUS_EN                    => XBUS_EN,
     XBUS_TIMEOUT               => XBUS_TIMEOUT,
     -- HPM --
-    HPM_NUM_CNTS                => HPM_NUM_CNTS
+    HPM_NUM_CNTS                => HPM_NUM_CNTS,
+    HPM_CNT_WIDTH               => HPM_CNT_WIDTH
   )
   port map (
     -- Global control --
